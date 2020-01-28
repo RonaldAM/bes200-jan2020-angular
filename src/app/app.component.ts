9b97555cc2540ee714958bb14bed152d986e5a48
RonaldAM <wholesomevalues@gmail.com>
2020-01-28 15:00:06 +0000

    done with demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'libraryApiClient';
+
+  response$: Observable<BooksResponse>;
+  constructor(private client: HttpClient){}
+
+  getThem(){
+    this.client.get<BooksResponse>('http://localhost:1337/books').subscribe(r=>console.log(r));
+  }
+
+}
+interface BooksResponse{
+
+}
+interface BooksResponseItem{
+  id: number,
+  title: string,
+  author: string;
 }
